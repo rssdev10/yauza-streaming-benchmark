146757bf0353c69877956d32a8cc7a5aec8f5b65
rssdev10 <rssdev10@gmail.com>
2016-05-24 16:32:51 +0200

    common: refactored data generator

--- benchmark-flink/src/test/java/helpers/TestDataGenerator.java
+++ benchmark-flink/src/test/java/helpers/TestDataGenerator.java
@@ -1,49 +1,11 @@
 package helpers;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
-
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
-import com.google.gson.Gson;
-
-import yauza.benchmark.common.Event;
+import yauza.benchmark.common.helpers.DummyEvent;
 
 public class TestDataGenerator {
-    private static Gson gson = new Gson();
-
-    private static final int uNum = 30;
-    private static final int percentageChangedSessions = 20;
-    private static final int percentagePurchase = 10;
-
-    private static final int maxPrice = 10000; // in cents
-
-    private static long timestamp = 0;
-    
-    private static List<String> provider = new ArrayList<String>(){
-        private static final long serialVersionUID = -2650978174049138472L;
-    {
-        add("play.google");
-        add("app.apple");
-        add("other.samsung");
-        add("other.microsoft");
-    }};
 
     private static final int eventsNum = 1000;
-    private static final Random rand = new Random();
-
-    private static final List<String> uId = new ArrayList<String>() {
-        private static final long serialVersionUID = -2650978174049138472L;
-        {
-            for (int i = 0; i < uNum; i++) {
-                add(UUID.randomUUID().toString());
-            }
-        }
-    };
 
     public static class UserEventsProducer implements SourceFunction<String> {
 
@@ -55,7 +17,7 @@ public class TestDataGenerator {
             // isRunning = true;
             // while (isRunning) {
             for (int i = 0; i < eventsNum; i++) {
-                String element = gson.toJson(dummyEvent());
+                String element = DummyEvent.generateJSON();
                 // the source runs, isRunning flag should be checked frequently
                 if (element != null)
                     sourceContext.collect(element);
@@ -72,35 +34,5 @@ public class TestDataGenerator {
 
     static public SourceFunction<String> getDatastream() {
         return new UserEventsProducer();
-    }
-
-    static public Event dummyEvent() {
-        Event event = new Event();
-        event.setUserId(uId.get(rand.nextInt(uNum)));
-
-        event.setSessionId(event.getUserId() +
-                Integer.toString(rand.nextInt(100) < percentageChangedSessions ? 1 : 0));
-
-        if (rand.nextInt(100) < percentagePurchase) {
-            event.setProvider(provider.get(rand.nextInt(provider.size())));
-            event.setReceiptId(UUID.randomUUID().toString());
-            event.setPrice(new Long(rand.nextInt(maxPrice)));
-        }
-
-        final SimpleDateFormat sdf = new SimpleDateFormat(Event.eventTimeFormat);
-        event.setTimestamp(sdf.format(getNextTimestamp()));
-        return event;
-    }
-
-    static private Object lock = new Object();
-    public static long getNextTimestamp() {
-        synchronized (lock) {
-            if (timestamp == 0) {
-                timestamp = new Date().getTime();
-            } else {
-                timestamp += (1 + rand.nextInt(100)) * 1000;
-            }
-        }
-        return timestamp;
     }
 }
---
+++ common/src/main/java/yauza/benchmark/common/helpers/DummyEvent.java
@@ -0,0 +1,82 @@
+package yauza.benchmark.common.helpers;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+
+import yauza.benchmark.common.Event;
+
+public class DummyEvent {
+    private static Gson gson = new Gson();
+
+    private static final int uNum = 30;
+    private static final int percentageChangedSessions = 20;
+    private static final int percentagePurchase = 10;
+
+    private static final int maxPrice = 10000; // in cents
+
+    private static long timestamp = 0;
+    
+    private static List<String> provider = new ArrayList<String>(){
+        private static final long serialVersionUID = -2650978174049138472L;
+    {
+        add("play.google");
+        add("app.apple");
+        add("other.samsung");
+        add("other.microsoft");
+    }};
+
+    private static final Random rand = new Random();
+
+    private static final List<String> uId = new ArrayList<String>() {
+        private static final long serialVersionUID = -2650978174049138472L;
+        {
+            for (int i = 0; i < uNum; i++) {
+                add(UUID.randomUUID().toString());
+            }
+        }
+    };
+
+    static public String eventToString(Event event) {
+        return gson.toJson(DummyEvent.generate());
+    }
+
+    static public Event generate() {
+        Event event = new Event();
+        event.setUserId(uId.get(rand.nextInt(uNum)));
+
+        event.setSessionId(event.getUserId() +
+                Integer.toString(rand.nextInt(100) < percentageChangedSessions ? 1 : 0));
+
+        if (rand.nextInt(100) < percentagePurchase) {
+            event.setProvider(provider.get(rand.nextInt(provider.size())));
+            event.setReceiptId(UUID.randomUUID().toString());
+            event.setPrice(new Long(rand.nextInt(maxPrice)));
+        }
+
+        final SimpleDateFormat sdf = new SimpleDateFormat(Event.eventTimeFormat);
+        event.setTimestamp(sdf.format(getNextTimestamp()));
+        return event;
+    }
+
+    public static String generateJSON() {
+        return eventToString(generate());
+    }
+
+    static private Object lock = new Object();
+    public static long getNextTimestamp() {
+        synchronized (lock) {
+            if (timestamp == 0) {
+                timestamp = new Date().getTime();
+            } else {
+                timestamp += (1 + rand.nextInt(100)) * 1000;
+            }
+        }
+        return timestamp;
+    }
+}
