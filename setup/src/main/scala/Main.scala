7670eedf29fb7b3fec8f9a03d556ce1536d6cb8a
rssdev10 <rssdev10@gmail.com>
2016-06-01 19:38:32 +0200

    setup: added scenarious

--- setup/src/main/scala/Main.scala
+++ setup/src/main/scala/Main.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 import scala.language.postfixOps
 import scala.sys.process._
 
-object Main extends App {
+object Main {
   private val apacheMirror = getApacheMirror
 
   //println("ls -l" !)
@@ -25,11 +25,11 @@ object Main extends App {
       s"""$apacheMirror/flink/flink-${VER("flink")}"""
     ) {
       override def start: Unit = {
-        println(s"""$dirName/bin/start-local.sh""" !!)
+        startIfNeeded("org.apache.flink.runtime.jobmanager.JobManager", "Flink", 1, s"""$dirName/bin/start-local.sh""")
       }
 
       override def stop: Unit = {
-        println(s"""$dirName/bin/stop-local.sh""" !!)
+        s"""$dirName/bin/stop-local.sh""" !
       }
     },
 
@@ -73,18 +73,72 @@ object Main extends App {
     }
   )
 
-  // download all products
-  products.foreach{case (k,v) => v.downloadAndUntar()}
+  val scenario: Map[String, () => Unit] = Map(
+    "setup" -> (() => {
+      //println(apacheMirror)
+      // download all products
+      products.foreach { case (k, v) => if (v.urlPath.nonEmpty) v.downloadAndUntar() }
+    }),
+    "test_flink" -> (() => {
+      // try to run Flink
+      products("flink").start
+      Thread sleep 30000
+      products("flink").stop
+    })
+  )
 
-  // try to run Flink
-  products("flink").start
-  products("flink").stop
-
-  //println(apacheMirror)
+  def main(args: Array[String]) {
+    if (args.length > 0 && scenario.contains(args(0))) {
+      scenario(args(0)).apply()
+    } else {
+      println(args.mkString(" "))
+      println(
+        """
+          |Select command:
+          | setup - download and unpack all files
+          | flink_test - run test with Flink
+          | ...
+          | """.stripMargin)
+    }
+  }
 
   private def getApacheMirror(): String = {
     val str = Source.fromURL("https://www.apache.org/dyn/closer.cgi").mkString
     ("""<strong>(.+)</strong>""".r).findFirstMatchIn(str).get.group(1)
+  }
+
+  def pidBySample(sample: String): String = try {
+    val line = ("ps -aef" !!)
+    if (line.nonEmpty)
+      line.split("\n").find(str => str.contains(sample)).head.split(" ").filter(_.nonEmpty).apply(1)
+    else
+      ""
+  } catch {
+    case _: Throwable => ""
+  }
+
+  def startIfNeeded(sample: String, name: String, sleepTime:Integer, args: String*): Unit = {
+    val pid = pidBySample(sample)
+    if (pid.nonEmpty) {
+      println( name + "is already running...")
+    } else {
+      args.mkString(" ").run()
+      Thread sleep(sleepTime * 1000)
+    }
+  }
+
+  def stopIfNeeded(sample: String, name: String): Unit = {
+    val pid = pidBySample(sample)
+    if (pid.nonEmpty) {
+      s"""kill $pid""".run()
+        Thread sleep 1000
+      val again = pidBySample(sample)
+      if (again.nonEmpty) {
+        s"""kill -9 $pid""".run()
+      }
+    } else {
+      println("No $name instance found to stop")
+    }
   }
 
   abstract class Product(val dirName: String, val fileName: String, val urlPath: String){
@@ -96,15 +150,18 @@ object Main extends App {
       println(s"Saving to $fileName")
 
       val file = new File(localFile)
-      if (file.exists() && file.length() > 0) {
+      val exists = file.exists()
+      if (exists && file.length() > 0) {
         println(s"Using cached File $fileName")
       } else {
-        new File("download-cache").mkdir
-        new URL(url) #> new File(localFile) !!
+        if (exists) file.delete()
+        else new File("download-cache").mkdir
+
+        new URL(url) #> new File(localFile) !
       }
 
       val tar = "tar -xzvf " + System.getProperty("user.dir") + "/" + localFile
-      println(tar !!)
+      tar !
     }
 
     def start : Unit
