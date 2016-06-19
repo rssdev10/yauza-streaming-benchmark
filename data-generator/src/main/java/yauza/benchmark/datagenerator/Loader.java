0680a847317cd8c6f9a43d0a6ac3599291e7eaf3
rssdev10 <rssdev10@gmail.com>
2016-06-19 17:19:12 +0200

    datagenerator: refactoring processing of incorrect properties

--- data-generator/src/main/java/yauza/benchmark/datagenerator/Loader.java
+++ data-generator/src/main/java/yauza/benchmark/datagenerator/Loader.java
@@ -24,22 +24,11 @@ public class Loader {
         String dataFile = parameterTool.get("datafile", "/yauza-benchmark/datafile.json");
 
         if (parameterTool.getNumberOfParameters() < 1) {
-            System.out.println(
-                    "Missing parameters!\n" +
-                            "Usage:\n" +
-                    " data-generator --mode generate_file" +
-                            " --hdfs hdfs://localhost:9000" +
-                            " --datafile datafile.json" +
-
-                    " data-generator --mode load_to_kafka" +
-                            " --topic <Kafka topic>" +
-                            " --bootstrap.servers <kafka brokers>" +
-                            " --hdfs hdfs://localhost:9000" +
-                            " --datafile datafile.json\n"
-            );
+            printHelpMessage();
             System.exit(1);
         }
-        String mode = parameterTool.get("mode", "load_to_kafka");
+
+        String mode = parameterTool.get("mode", "");
         if (mode.equals("generate_file")) {
             // generate data and write into HDFS
             new HdfsWriter().generate(hdfsPath, dataFile);
@@ -52,12 +41,33 @@ public class Loader {
             Properties kafkaProps = parameterTool.getProperties();
             kafkaProps.remove("topic");
             kafkaProps.remove("mode");
+            kafkaProps.remove("hdfs");
+            kafkaProps.remove("datafile");
 
             DataStream<String> dataStream = env.readFile(new DataFileInputFormat(filePath), filePath);
             dataStream.addSink(new FlinkKafkaProducer09<>(parameterTool.getRequired("topic"), new SimpleStringSchema(),
                     kafkaProps));
 
             env.execute();
+        } else {
+            printHelpMessage();
         }
+        System.exit(1);
+    }
+
+    private static void printHelpMessage() {
+        System.out.println(
+                "Missing parameters!\n" +
+                        "Usage:\n" +
+                        " data-generator --mode generate_file" +
+                        " --hdfs hdfs://localhost:9000" +
+                        " --datafile datafile.json" +
+
+                        " data-generator --mode load_to_kafka" +
+                        " --topic <Kafka topic>" +
+                        " --bootstrap.servers <kafka brokers>" +
+                        " --hdfs hdfs://localhost:9000" +
+                        " --datafile datafile.json\n"
+        );
     }
 }
