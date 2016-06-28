d41ba0f618c4210c1bbe05c4eded2474840bce60
rssdev10 <rssdev10@gmail.com>
2016-06-28 16:55:48 +0200

    spark: number of distinct id by means of spark without any statistic

--- benchmark-spark/src/main/java/yauza/benchmark/spark/UniqItems.java
+++ benchmark-spark/src/main/java/yauza/benchmark/spark/UniqItems.java
@@ -3,8 +3,12 @@ package yauza.benchmark.spark;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import scala.Tuple2;
 import yauza.benchmark.common.Product;
 import yauza.benchmark.common.Statistics;
 import yauza.benchmark.common.accessors.FieldAccessorString;
@@ -33,44 +37,14 @@ public class UniqItems {
      * @return new chained stream
      */
     public static JavaDStream<String> transform(JavaDStream<Event> eventStream, FieldAccessorString fieldAccessor) {
-        JavaDStream<Event> uniqUsersWin = eventStream.repartition(SparkBenchmark.partNum).window(Duration.apply(10 * 1000));
 
-/*        uniqUsers = uniqUsersWin.rdd
-                .
-                .fold(new UniqAggregator(), new FoldFunction<Event, UniqAggregator>() {
-                    private static final long serialVersionUID = -6020094091742548382L;
-
-                    @Override
-                    public UniqAggregator fold(UniqAggregator accumulator, Event value) throws Exception {
-                        accumulator.uniqIds.add(fieldAccessor.apply(value));
-
-                        accumulator.registerEvent(value);
-
-                        return accumulator;
-                    }
-                });
-
-        AllWindowedStream<UniqAggregator, TimeWindow> combinedUniqNumStream =
-                uniqUsers
-                        .timeWindowAll(Time.seconds(FlinkApp.emergencyTriggerTimeout))
-                        .trigger(PurgingTrigger.of(CountOrTimeTrigger.of(FlinkApp.partNum)));
-
-        return combinedUniqNumStream.fold(new ProductAggregator(),
-                new FoldFunction<UniqAggregator, ProductAggregator>() {
-                    private static final long serialVersionUID = 7167358208807786523L;
-
-                    @Override
-                    public ProductAggregator fold(ProductAggregator accumulator, UniqAggregator value) throws Exception {
-                        System.out.println(value.toString());
-                        accumulator.value += value.uniqIds.size();
-
-                        accumulator.summarize(value);
-
-                        return accumulator;
-                    }*/
-
-        return eventStream.map(x -> {
+        JavaPairDStream<String, Long> distinct = eventStream.map(event -> fieldAccessor.apply(event)).countByValue();
+        return distinct
+                .reduce((Function2<Tuple2<String, Long>, Tuple2<String, Long>, Tuple2<String, Long>>)
+                    (v1, v2) -> new Tuple2<String, Long>(null, v1._2 + v2._2))
+                .map(x -> {
             Product product = new Product("UniqItems", x.toString());
+            //product.setStatistics(x);
             return product.toString();
         });
     }
