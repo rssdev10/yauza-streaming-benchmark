92e7660d124e5e1d402ba873b4eb38b90c6ac4a1
rssdev10 <rssdev10@gmail.com>
2016-07-10 20:26:43 +0200

    added javadoc to kafka sink

--- benchmark-spark/src/main/java/yauza/benchmark/spark/KafkaSink.java
+++ benchmark-spark/src/main/java/yauza/benchmark/spark/KafkaSink.java
@@ -6,6 +6,23 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.io.Serializable;
 import java.util.Properties;
 
+/**
+ * This class implements serializable kafka sink for Spark.
+ *
+ * Usage:
+ * <PRE>
+ *    Broadcast<KafkaSink> kafkaSink =
+ *      jssc.sparkContext().broadcast(new KafkaSink(kafkaProps));
+ *
+ *    stream.foreachRDD(rdd -> {
+ *      rdd.foreachPartition( artitionOfRecords -> {
+ *        partitionOfRecords.forEachRemaining(x -> {
+ *          kafkaSink.value().send(topic,x);
+ *        });
+ *      });
+ *    });
+ * </PRE>
+ */
 public class KafkaSink implements Serializable {
     private Properties config = null;
     private KafkaProducer<String, String> producer = null;
@@ -23,12 +40,12 @@ public class KafkaSink implements Serializable {
                 init(config);
             }
         } else {
-            producer.send(new ProducerRecord(topic, value));
+            producer.send(new ProducerRecord<>(topic, value));
         }
     }
 
     private void init(Properties config) {
-        producer = new KafkaProducer<String, String>(config);
+        producer = new KafkaProducer<>(config);
         Runtime.getRuntime().addShutdownHook( new Thread () {
             @Override
             public void run() {
