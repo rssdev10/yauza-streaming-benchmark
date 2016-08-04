bb916e920141a3b8e205e0b09d729cead22bd5a4
rssdev10 <rssdev10@gmail.com>
2016-08-04 20:16:15 +0200

    data-generator: added limitation per sec for hdfs uploading. Doesn't calculate common time for different partitions

--- data-generator/src/main/java/yauza/benchmark/datagenerator/Loader.java
+++ data-generator/src/main/java/yauza/benchmark/datagenerator/Loader.java
@@ -1,5 +1,6 @@
 package yauza.benchmark.datagenerator;
 
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -17,7 +18,8 @@ import java.util.Properties;
  *
  */
 public class Loader {
-    Logger LOG = LoggerFactory.getLogger(Loader.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Loader.class);
+    private static final long INTERVAL_MS = 1000;
 
     public static void main(final String[] args) throws Exception {
         ParameterTool parameterTool = ParameterTool.fromArgs(args);
@@ -52,12 +54,44 @@ public class Loader {
 
             Properties kafkaProps = config.getKafkaProperties();
 
-            DataStream<String> dataStream = env.readFile(new DataFileInputFormat(filePath), filePath);
-//            dataStream.addSink(new FlinkKafkaProducer09<>(parameterTool.getRequired("topic"), new SimpleStringSchema(),
-//                    kafkaProps));
-            dataStream.addSink(new FlinkKafkaProducer08<>(parameterTool.getRequired("topic"), new SimpleStringSchema(),
-                    kafkaProps));
+            final int limitPerSecond =
+                    Integer.parseInt(config.getProperties().getProperty(
+                            "benchmark.messages.per.second", Long.toString(HdfsWriter.eventsNum)));
 
+            DataStream<String> dataStream = env.readFile(new DataFileInputFormat(filePath), filePath);
+            if (limitPerSecond > 0) {
+                dataStream = dataStream.map(new RichMapFunction<String, String>() {
+                    private long time = System.currentTimeMillis();
+                    private int messages = 0;
+
+                    @Override
+                    public String map(String value) throws Exception {
+                        // limit per partition only
+                        if (messages >= limitPerSecond) {
+                            long curTime = System.currentTimeMillis();
+                            if (Math.abs(curTime - time) < INTERVAL_MS) {
+                                try {
+                                    Thread.sleep(INTERVAL_MS - Math.abs(curTime - time));
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            messages = 0;
+                            time = curTime;
+                        }
+                        messages++;
+                        return value;
+                    }
+                });
+            }
+
+//            dataStream.addSink(new FlinkKafkaProducer09<>(config.getProperty("topic", Config.INPUT_TOPIC_NAME),
+//                    new SimpleStringSchema(),
+//                    kafkaProps));
+
+            dataStream.addSink(new FlinkKafkaProducer08<>(config.getProperty("topic", Config.INPUT_TOPIC_NAME),
+                    new SimpleStringSchema(),
+                    kafkaProps));
             //dataStream.print();
 
             env.execute();
