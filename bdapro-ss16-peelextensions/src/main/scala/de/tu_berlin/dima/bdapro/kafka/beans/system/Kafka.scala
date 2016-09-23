0e0a5031ea75632d94125bb58349f0d683274b2a
rssdev10 <rssdev10@gmail.com>
2016-09-23 21:11:37 +0300

    kafka: tested for localhost only

--- bdapro-ss16-peelextensions/src/main/scala/de/tu_berlin/dima/bdapro/kafka/beans/system/Kafka.scala
+++ bdapro-ss16-peelextensions/src/main/scala/de/tu_berlin/dima/bdapro/kafka/beans/system/Kafka.scala
@@ -1,17 +1,12 @@
 package de.tu_berlin.dima.bdapro.kafka.beans.system
 
-import java.io.PrintWriter
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths, StandardOpenOption}
 import java.util.regex.Pattern
 
 import com.samskivert.mustache.Mustache
-import org.peelframework.core.beans.experiment.Experiment.Run
 import org.peelframework.core.beans.system.Lifespan.Lifespan
 import org.peelframework.core.beans.system.{DistributedLogCollection, System}
 import org.peelframework.core.config.{Model, SystemConfig}
 import org.peelframework.core.util.shell
-import resource._
 
 import scala.collection.JavaConverters._
 import scala.collection.Seq
@@ -42,9 +37,7 @@ class Kafka(
   // LogCollection.
   // ---------------------------------------------------
 
-  override def hosts: Seq[String]  = config.getStringList(s"system.$configKey.config.hosts").asScala
-
-  var processes: Seq[ProcessDescriptor] = Seq.empty[ProcessDescriptor]
+  override def hosts: Seq[String] = config.getStringList(s"system.$configKey.config.hosts").asScala
 
   override protected def logFilePatterns(): Seq[Regex] = {
     hosts.map(Pattern.quote).flatMap(node => Set(
@@ -59,10 +52,10 @@ class Kafka(
   override def configuration() = SystemConfig(config, {
     val conf = config.getString(s"system.$configKey.path.config")
     List(
-      SystemConfig.Entry[Model.Yaml](          // render using the 'Model.Yaml' controller
-        s"system.$configKey.config.yaml",      // use this config namespace as a 'model'
-        s"$conf/kafka-conf.yaml",              // instantiate the config file
-        templatePath("conf/kafka-conf.yaml"),  // use this template to render the file
+      SystemConfig.Entry[Model.Yaml](             // render using the 'Model.Yaml' controller
+        s"system.$configKey.server.properties",   // use this config namespace as a 'model'
+        s"$conf/server.properties",               // instantiate the config file
+        templatePath("config/server.properties"), // use this template to render the file
         mc
       )
     ) // return a list of configuration files to render on setup
@@ -70,85 +63,83 @@ class Kafka(
 
   override protected def start(): Unit = {
     val kafkaPath = config.getString(s"system.$configKey.path.home")
+    val kafkaTmp = config.getString(s"system.$configKey.path.kafka.tmp")
     val logDir = config.getString(s"system.$configKey.path.log")
+    val timeout: Int = config.getString(s"system.$configKey.startup.timeout").toInt
 
     logger.info("Starting kafka processes on all hosts")
-    val futureProcessDescriptors: Future[Seq[ProcessDescriptor]] = Future.traverse(hosts)(host => Future {
+    val futureProcessDescriptors: Future[Seq[(String, Int)]] = Future.traverse(hosts)(host => Future {
       val cmd: String = Seq(
+        s"rm -rf ${kafkaTmp};",                  // TODO: make cleaing of Kafka dir more safe for incorrent dir
+        s"export LOG_DIR=${logDir};",
         s"${kafkaPath}/bin/kafka-server-start.sh",
-        s"${kafkaPath}/config/server.properties"
+        s"${kafkaPath}/config/server.properties" // TODO: specify properties file unique for each node
       ).mkString(" ")
 
-      val pid = (shell !! s""" ssh $host 'rm -f "$logDir"; nohup $cmd >/dev/null 2>/dev/null & echo $$!' """).trim
-      logger.info(s"Kafka started on host '$host' with PID $pid")
+      shell ! s""" ssh $host 'nohup $cmd >/dev/null 2>/dev/null & echo $$!' """
 
-      ProcessDescriptor(host, pid.toInt)
+      Thread.sleep(timeout * 1000)
+
+      val pid = sshFindKafkaPid(host)
+
+      (host, pid)
     })
-    setProcesses(Await.result(futureProcessDescriptors, Math.max(30, 5 * hosts.size).seconds))
-    createTopic()
+    Await.result(futureProcessDescriptors, Math.max(30, 5 * hosts.size).seconds).foreach { case (host, pid) =>
+      if (pid > 0)
+        logger.info(s"Kafka started on host '$host' with PID $pid")
+      else
+        logger.error(s"Kafka didn't start on host '$host'")
+    }
+
+    createTopic("test-topic", 2) // TODO: remove after testing
   }
 
   override protected def stop(): Unit = {
-  }
-
-  private def stopProcesses(runOpt: Option[Run[System]]): Unit = {
     val user = config.getString(s"system.$configKey.user")
-
-    val pidFle = Paths.get(config.getString(s"system.$configKey.path.pids"))
 
     logger.info("Stopping Kafka processes on all hosts")
     val futureKillProcess = Future.traverse(hosts)(host => Future {
-      shell ! s"""
-         |ssh $$host << SSHEND
-         |  PID==`ps -aef | grep "[k]afka.Kafka" | awk '{print  $$2}' |  head -1`
-         |
-         |  if [[ "$$PID" -ne "" ]]; then
-         |    kill "$$PID"
-         |    sleep 3
-         |  fi
-         |
-         |  PID==`ps -aef | grep "[k]afka.Kafka" | awk '{print  $$2}' |  head -1`
-         |
-         |  if [[ "$$PID" -ne "" ]]; then
-         |    kill -9 "$$PID"
-         |  fi
-         |
-         |  exit
-         |SSHEND
-         |""".stripMargin
+      var pid = sshFindKafkaPid(host)
+      if (pid > 0) {
+        shell ! s"ssh $host kill $pid"
+
+        Thread sleep(5000)
+
+        pid = sshFindKafkaPid(host)
+        if (pid > 0) {
+          shell ! s"ssh $host kill -9 $pid"
+        }
+      }
       logger.info(s"Kafka stopped on host '${host}'")
     })
     Await.result(futureKillProcess, Math.max(30, 5 * hosts.size).seconds)
   }
 
-  override def isRunning: Boolean =
-    processes.size == hosts.size
+  override def isRunning: Boolean = {
+    val futureProcessDescriptors: Future[Seq[Int]] = Future.traverse(hosts)(host => Future {
+      sshFindKafkaPid(host)
+    })
+    Await.result(futureProcessDescriptors, Math.max(30, 5 * hosts.size).seconds).exists(_ > 0)
+  }
 
-  // ---------------------------------------------------
+    // ---------------------------------------------------
   // Helper methods.
   // ---------------------------------------------------
 
-  private def setProcesses(processes: Seq[ProcessDescriptor]) = {
-    val pidFle = Paths.get(config.getString(s"system.$configKey.path.pids"))
-
-    import StandardCharsets.UTF_8
-    import StandardOpenOption.{CREATE, TRUNCATE_EXISTING, WRITE}
-
-    for {
-      buf <- managed(Files.newBufferedWriter(pidFle, UTF_8, CREATE, WRITE, TRUNCATE_EXISTING))
-      out <- managed(new PrintWriter(buf))
-    } for (desc <- processes) {
-      out.write(s"$desc\n")
+  def sshFindPidByTemplate(host:String, template: String):Int = {
+    val pid = shell.!!(s"""ssh $host "ps -aef | grep "$template" | awk '{print \\$$2}' | head -1"""").trim
+    try {
+      pid.toInt
+    } catch {
+      case e: NumberFormatException => 0
     }
-
-    this.processes = processes
   }
 
-  private def createTopic():Unit = {
+  private def sshFindKafkaPid(host:String):Int = sshFindPidByTemplate(host, "[k]afka.Kafka")
+
+  def createTopic(inputTopic:String, partitions:Int = 1):Unit = {
     val dirName = config.getString(s"system.$configKey.path.home")
-    val zk_connections = config.getString(s"system.$configKey.zookeeper")
-    val partitions: Int = config.getString(s"system.$configKey.partitions").toInt
-    val inputTopic = config.getString(s"system.$configKey.inputTopic")
+    val zk_connections = config.getString(s"system.$configKey.server.properties.zookeeper.connect")
 
     val count = (shell !! s"""$dirName/bin/kafka-topics.sh --describe --zookeeper $zk_connections --topic $inputTopic 2>/dev/null""")
       .split("\n").find(str => str.contains(inputTopic)).size
@@ -156,18 +147,7 @@ class Kafka(
     if (count == 0) {
       shell !! s"""$dirName/bin/kafka-topics.sh --create --zookeeper $zk_connections --replication-factor 1 --partitions $partitions --topic $inputTopic"""
     } else {
-      println(s"Kafka topic $inputTopic already exists")
+      logger.info(s"Kafka topic $inputTopic already exists")
     }
-  }
-}
-
-case class ProcessDescriptor(host: String, pid: Int)
-
-object ProcessDescriptor {
-  val ProcessDescriptorRegex = "ProcessDescriptor\\((.*),(\\d+)\\)".r
-
-  def unapply(s: String): Option[ProcessDescriptor] = s match {
-    case ProcessDescriptorRegex(host, pid) => Some(new ProcessDescriptor(host, pid.toInt))
-    case _ => None
   }
 }
