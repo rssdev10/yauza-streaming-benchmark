903272b154d50b5160e308a64d79f737dd824425
rssdev10 <rssdev10@gmail.com>
2016-10-12 21:55:51 +0300

    peel: added bundle content

---
+++ yauza-benchmark-bundle/src/main/resources/config/experiments.scala
@@ -0,0 +1,33 @@
+package config
+
+import org.springframework.context.annotation._
+import org.springframework.context.{ApplicationContext, ApplicationContextAware}
+
+/** Experiments definitions for the 'yauza-benchmark' bundle. */
+@Configuration
+@ComponentScan( // Scan for annotated Peel components in the 'yauza.benchmark' package
+  value = Array("yauza.benchmark"),
+  useDefaultFilters = false,
+  includeFilters = Array[ComponentScan.Filter](
+    new ComponentScan.Filter(value = Array(classOf[org.springframework.stereotype.Service])),
+    new ComponentScan.Filter(value = Array(classOf[org.springframework.stereotype.Component]))
+  )
+)
+@ImportResource(value = Array(
+  "classpath:peel-core.xml",
+  "classpath:peel-extensions.xml"
+))
+@Import(value = Array(
+  classOf[org.peelframework.extensions], // custom system beans
+  classOf[config.fixtures.systems],      // custom system beans
+  classOf[config.fixtures.wordcount]     // wordcount experiment beans
+))
+class experiments extends ApplicationContextAware {
+
+  /* The enclosing application context. */
+  var ctx: ApplicationContext = null
+
+  def setApplicationContext(ctx: ApplicationContext): Unit = {
+    this.ctx = ctx
+  }
+}
---
+++ yauza-benchmark-bundle/src/main/resources/config/fixtures/systems.scala
@@ -0,0 +1,74 @@
+package config.fixtures
+
+import de.tu_berlin.dima.bdapro.kafka.beans.system.Kafka
+import de.tu_berlin.dima.bdapro.storm.beans.system.Storm
+
+import com.samskivert.mustache.Mustache
+import org.peelframework.core.beans.system.{Lifespan, System}
+import org.peelframework.flink.beans.system.Flink
+import org.peelframework.hadoop.beans.system.HDFS2
+import org.peelframework.spark.beans.system.Spark
+import org.peelframework.zookeeper.beans.system.Zookeeper
+import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.context.{ApplicationContext, ApplicationContextAware}
+
+/** System beans for the 'bdapro-ss16' bundle. */
+@Configuration
+class systems extends ApplicationContextAware {
+
+  /* The enclosing application context. */
+  var ctx: ApplicationContext = null
+
+  def setApplicationContext(ctx: ApplicationContext): Unit = {
+    this.ctx = ctx
+  }
+
+  // ---------------------------------------------------
+  // Systems
+  // ---------------------------------------------------
+
+  @Bean(name = Array("flink-1.0.3"))
+  def `flink-1.0.3`: Flink = new Flink(
+    version      = "1.0.3",
+    configKey    = "flink",
+    lifespan     = Lifespan.EXPERIMENT,
+    dependencies = Set(ctx.getBean("hdfs-2.7.1", classOf[HDFS2])),
+    mc           = ctx.getBean(classOf[Mustache.Compiler])
+  )
+
+  @Bean(name = Array("spark-1.4.0"))
+  def `spark-1.4.0`: Spark = new Spark(
+    version      = "1.4.0",
+    configKey    = "spark",
+    lifespan     = Lifespan.EXPERIMENT,
+    dependencies = Set(ctx.getBean("hdfs-2.7.1", classOf[HDFS2])),
+    mc           = ctx.getBean(classOf[Mustache.Compiler])
+  )
+
+  @Bean(name = Array("spark-1.6.0"))
+  def `spark-1.6.0`: Spark = new Spark(
+    version      = "1.6.0",
+    configKey    = "spark",
+    lifespan     = Lifespan.EXPERIMENT,
+    dependencies = Set(ctx.getBean("hdfs-2.7.1", classOf[HDFS2])),
+    mc           = ctx.getBean(classOf[Mustache.Compiler])
+  )
+
+  @Bean(name = Array("kafka-0.8.2.2"))
+  def `kafka-0.8.2.2`: Kafka = new Kafka(
+    version      = "0.8.2.2",
+    configKey    = "kafka",
+    lifespan     = Lifespan.EXPERIMENT,
+    dependencies = Set(ctx.getBean("zookeeper-3.4.5", classOf[Zookeeper])),
+    mc           = ctx.getBean(classOf[Mustache.Compiler])
+  )
+
+  @Bean(name = Array("storm-1.0.2"))
+  def `storm-1.0.2`: Storm = new Storm(
+    version      = "1.0.2",
+    configKey    = "storm",
+    lifespan     = Lifespan.EXPERIMENT,
+    dependencies = Set(ctx.getBean("zookeeper-3.4.5", classOf[Zookeeper])),
+    mc           = ctx.getBean(classOf[Mustache.Compiler])
+  )
+}
